3c4dd87596cc848a1d5b897b6d56876a9b02e388
yoonjae <backendINFJ@github.com>
2024-06-14 10:36:38 +0900

    #1/TestCode - Feat:FixtureMonkey를 이용한 UserEntity TestCode 기능 구현

--- src/test/java/december/spring/studywithme/entity/FixtureMonkeyUserTest.java
+++ src/test/java/december/spring/studywithme/entity/FixtureMonkeyUserTest.java
@@ -1,13 +1,10 @@
 package december.spring.studywithme.entity;
 
 import com.navercorp.fixturemonkey.FixtureMonkey;
-import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
 import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
-import com.navercorp.fixturemonkey.jackson.generator.JacksonArbitraryGenerator;
-import net.jqwik.api.Arbitraries;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.navercorp.fixturemonkey.api.experimental.JavaGetterMethodPropertySelector.javaGetter;
 import static org.assertj.core.api.BDDAssertions.then;
 
 
@@ -15,11 +12,12 @@ public class FixtureMonkeyUserTest {
 
 
     @Test
+    @DisplayName("회원가입 테스트")
     void createUser() {
 
         // given
         FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
-                .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)
+                .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE) // 객체 생성전략
                 .build();
 
         // when
@@ -37,4 +35,102 @@ public class FixtureMonkeyUserTest {
         then(user.getEmail()).isEqualTo("yj@gmail.com");
         then(user.getIntroduce()).isEqualTo("제발 좀 되라!!");
     }
+
+    @Test
+    @DisplayName("회원 상태 변경 - 탈퇴 회원")
+    void whithdrawUser() {
+
+        // given
+        FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
+                .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE) // 객체 생성전략
+                .build();
+
+        // when
+        User user = fixtureMonkey.giveMeBuilder(User.class)
+                .set("userType", UserType.UNVERIFIED)
+                .sample();
+        user.withdrawUser();
+
+        // then
+        then(user.getUserType()).isEqualTo(UserType.DEACTIVATED);
+    }
+
+    @Test
+    @DisplayName("회원 상태 변경 - 인증 회원")
+    void activeUser() {
+        // given
+        FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
+                .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE) // 객체 생성전략
+                .build();
+
+        // when
+        User user = fixtureMonkey.giveMeBuilder(User.class)
+                .set("userType", UserType.UNVERIFIED)
+                .sample();
+        user.ActiveUser();
+
+        // then
+
+        then(user.getUserType()).isEqualTo(UserType.ACTIVE);
+    }
+
+    @Test
+    @DisplayName("로그인시 리프레시 토큰 초기화")
+    void refreshTokenReset() {
+
+        // given
+        FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
+                .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE) // 객체 생성전략
+                .build();
+
+        // when
+        User user = fixtureMonkey.giveMeBuilder((User.class))
+                .set("refreshToken", "refreshToken")
+                .sample();
+
+        // then
+        then(user.getRefreshToken()).isEqualTo("refreshToken");
+    }
+
+    @Test
+    @DisplayName("프로필 수정")
+    void editProfile() {
+
+        // given
+        FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
+                .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE) // 객체 생성전략
+                .build();
+
+        // when
+        User user = fixtureMonkey.giveMeBuilder(User.class)
+                .set("name", "픽쳐몽키")
+                .set("introduce", "제발 되었으면 좋겠다.")
+                .sample();
+
+        user.editProfile("픽쳐몽키수정", "제발 되라~!!~");
+        // then
+        then(user.getName()).isEqualTo("픽쳐몽키수정");
+        then(user.getIntroduce()).isEqualTo("제발 되라~!!~");
+    }
+
+    @Test
+    @DisplayName("비밀번호 변경")
+    void changePassword() {
+
+        // given
+        FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
+                .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE) // 객체 생성전략
+                .build();
+
+        // when
+        User user = fixtureMonkey.giveMeBuilder(User.class)
+                .set("password", "1234")
+                .sample();
+
+        user.changePassword("1234");
+
+        // then
+        then(user.getPassword()).isEqualTo("1234");
+    }
 }
+
